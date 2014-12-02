e2c6307a1f7a2f167ce625bcc9b3fc40b3c4c51c
Raukonim <Raukonim@users.noreply.github.com>
2014-12-02 18:06:00 +0100

    first model created

---
+++ models.py
@@ -0,0 +1,20 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here
+
+class Review(models.Model):
+    title=models.CharField(max_length=200)
+    pub_date=models.DateTimeField('date published')
+    review_txt=models.CharField(max_length=3000)
+    review_points=models.DecimalField(default=0.0, max_digits=3,
+        decimal_places=1)
+    
+    def __unicode__(self):
+        return self.title
+
+    def was_published_recently(self):
+        return self.pub_date >= (timezone.now() - 
+            datetime.timedelta(days=1))
